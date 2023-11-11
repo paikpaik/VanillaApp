dfd7c4dcdecbc306529d130f378bbdaa0fa01124
paikpaik <woduf9311@gmail.com>
2023-11-11 18:03:20 +0900

    Add: localStorage 저장

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function handleLoginSubmit(e) {
   e.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  greeting.innerText = `Hello ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+
+function paintGreetings(value) {
+  greeting.innerText = `Hello ${value}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", handleLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if (savedUsername === null) {
+  // form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginSubmit);
+} else {
+  // greeting
+  paintGreetings(savedUsername);
+}
